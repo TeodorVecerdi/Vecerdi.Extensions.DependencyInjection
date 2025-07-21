6910dd0de7c364d76d012271f3c63029f6b220b0
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-07-21 12:44:14 +0300

    Add resolver that combines several others

---
+++ Infrastructure/TypeInjectorResolverCombiner.cs
@@ -0,0 +1,13 @@
+﻿namespace Vecerdi.Extensions.DependencyInjection.Infrastructure;
+
+public class TypeInjectorResolverCombiner(params ITypeInjectorResolver[] resolvers) : ITypeInjectorResolver {
+    public ITypeInjector? GetTypeInjector(Type type) {
+        foreach (var resolver in resolvers) {
+            var injector = resolver.GetTypeInjector(type);
+            if (injector != null)
+                return injector;
+        }
+
+        return null;
+    }
+}
