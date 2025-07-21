568e91bc3842e6ff29c9fe431a09847ea481741e
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-07-21 12:44:25 +0300

    Add resolver for source generated code

---
+++ Infrastructure/TypeInjectorResolverContext.cs
@@ -0,0 +1,5 @@
+﻿namespace Vecerdi.Extensions.DependencyInjection.Infrastructure;
+
+public abstract class TypeInjectorResolverContext : ITypeInjectorResolver {
+    public abstract ITypeInjector? GetTypeInjector(Type type);
+}
