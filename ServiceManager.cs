786170212b002d2112392777b5b01d7567469a87
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-08-02 15:45:04 +0300

    Add support for hosted services in the ServiceManager

--- ServiceManager.cs
+++ ServiceManager.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using UnityEngine;
 using Vecerdi.Extensions.DependencyInjection.Infrastructure;
 
@@ -13,6 +16,8 @@ public sealed class ServiceManager : MonoSingleton<ServiceManager>, IKeyedServic
     private static readonly List<Action<IConfigurationManager>> s_ConfigurationRegistrations = [];
     private static readonly List<Action<IServiceProvider>> s_PostInitializationActions = [];
     private IServiceProvider? m_ServiceProvider;
+    private readonly List<IHostedService> m_HostedServices = [];
+    private readonly CancellationTokenSource m_ApplicationStoppingCts = new();
 
     protected override void Awake() {
         base.Awake();
@@ -33,10 +38,14 @@ public sealed class ServiceManager : MonoSingleton<ServiceManager>, IKeyedServic
 
         s_PostInitializationActions.ForEach(action => action(m_ServiceProvider));
         s_PostInitializationActions.Clear();
+
+        _ = StartHostedServicesAsync();
     }
 
     protected override void OnDestroy() {
         base.OnDestroy();
+
+        _ = StopHostedServicesAsync();
 
         // ReSharper disable SuspiciousTypeConversion.Global
         if (m_ServiceProvider is IAsyncDisposable asyncDisposable) {
@@ -47,6 +56,7 @@ public sealed class ServiceManager : MonoSingleton<ServiceManager>, IKeyedServic
         // ReSharper restore SuspiciousTypeConversion.Global
 
         m_ServiceProvider = null;
+        m_ApplicationStoppingCts.Dispose();
     }
 
     public static void RegisterServices(Action<IServiceCollection, IConfigurationManager> configuration) {
@@ -84,5 +94,47 @@ public sealed class ServiceManager : MonoSingleton<ServiceManager>, IKeyedServic
 
         return m_ServiceProvider.GetKeyedServices(serviceType, serviceKey).FirstOrDefault()
             ?? throw new InvalidOperationException($"Service {serviceType} with key {serviceKey} is not registered.");
+    }
+
+    private async Task StartHostedServicesAsync() {
+        if (m_ServiceProvider is null)
+            throw new InvalidOperationException("ServiceManager is not initialized yet");
+
+        var logger = m_ServiceProvider.GetRequiredService<ILogger<ServiceManager>>();
+        try {
+            var hostedServices = m_ServiceProvider.GetServices<IHostedService>();
+            m_HostedServices.AddRange(hostedServices);
+
+            foreach (var hostedService in m_HostedServices) {
+                await hostedService.StartAsync(m_ApplicationStoppingCts.Token);
+            }
+        } catch (Exception ex) {
+            logger.LogError(ex, "Error starting hosted services");
+        }
+    }
+
+    private async Task StopHostedServicesAsync() {
+        if (m_ServiceProvider is null)
+            throw new InvalidOperationException("ServiceManager is not initialized yet");
+        if (!m_HostedServices.Any())
+            return;
+
+        var logger = m_ServiceProvider.GetRequiredService<ILogger<ServiceManager>>();
+        try {
+            m_ApplicationStoppingCts.Cancel();
+
+            // Stop services in reverse order
+            for (var i = m_HostedServices.Count - 1; i >= 0; i--) {
+                try {
+                    await m_HostedServices[i].StopAsync(CancellationToken.None);
+                } catch (Exception ex) {
+                    logger.LogError(ex, "Error stopping hosted service {Name}", m_HostedServices[i].GetType().Name);
+                }
+            }
+
+            m_HostedServices.Clear();
+        } catch (Exception ex) {
+            logger.LogError(ex, "Error stopping hosted services");
+        }
     }
 }
